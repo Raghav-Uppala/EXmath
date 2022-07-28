7c48ec3439481f57b1180120ee0e86eb6415508e
Raghav Uppala <Ruppala20@gmail.com>
2022-07-28 14:48:27 +0530

    updated __init__

--- Exmath/__init__.py
+++ Exmath/__init__.py
@@ -1,4 +1,5 @@
 from math import *
 from .math_func import roundx
 from .stats import meanx, medianx, modex
-from .coord import distance2P2Dx, gradient2P2Dx, distance2P3Dx, gradientFAx, midpoint2Px, perpendicularBisector2Px, lineFrom2Px
+from .coord import distance2P2Dx, gradient2P2Dx, distance2P3Dx, gradientFAx, midpoint2Px, perpendicularBisector2Px, lineFrom2Px, collinearTest3P2Dx
+from .algebra import quadraticRoots
